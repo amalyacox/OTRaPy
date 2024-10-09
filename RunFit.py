a8b898cd1672840f9dde18194bd7d40f859d63e5
Amalya <88465999+amalyacox@users.noreply.github.com>
2024-10-09 15:37:43 -0700

    initial commit

---
+++ RunFit.py
@@ -0,0 +1,25 @@
+
+import json 
+from RamanSolver import RamanSolver
+import sys
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+
+with open(infile, 'r') as f:
+    input_dict = json.load(f)
+
+solver = RamanSolver()
+for key in input_dict['init']: 
+    setattr(solver, key, input_dict['init'][key])
+
+solver.iter_solve(**input_dict['iter_solve'])
+
+results = {'kx':solver.kx_solved,
+'ky':solver.ky_solved,
+'g':solver.g_solved}
+
+with(outfile, 'w') as f: 
+    json.dump(results, f)
+
+
