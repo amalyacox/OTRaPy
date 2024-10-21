a3a8095427d4c4e14b09c162250c47a2a29753cd
Amalya <amalyajohnson17@gmail.com>
2024-10-21 16:31:43 -0700

    reconfiguring build

--- setup.py
+++
@@ -1,7 +0,0 @@
-#Author : Amalya C. Johnson
-#email  : amalyaj@stanford.edu
-
-from setuptools import setup, find_packages
-
-setup(name = "OTRaPy",version="1.0", packages=find_packages(where="OTRaPy"), package_dir = {'':"OTRaPy"})
-
