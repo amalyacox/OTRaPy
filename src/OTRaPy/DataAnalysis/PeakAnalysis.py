c08ef22200068714fd122230b88ef1d9d498b1ed
Amalya <88465999+amalyacox@users.noreply.github.com>
2025-04-12 00:38:19 -0700

    formatting, documenting

--- src/OTRaPy/DataAnalysis/PeakAnalysis.py
+++ src/OTRaPy/DataAnalysis/PeakAnalysis.py
@@ -16,19 +16,26 @@ import lmfit as lm
 from OTRaPy.utils import *
 import h5py as h5
 
-# Functions for fitting data and stuff
+# Functions for fitting spectral data
 
 
 def wv_range(energies, wv1, wv2):
     """
     In self.energies, find index closest to specific start/stop points
 
-    Args:
-        wv1 (float or int) : first energy in range
-        wv2 (float or int) : last energy in range
+    Parameters
+    ----------
+    energies : array like
+        1D array of energies
+    wv1 : float or int
+        first energy in range
+    wv2 : float or int
+        last energy in range
 
-    Returns:
-        start, stop (int): indices in self.energies to get nearest to wv1, wv2
+    Returns
+    -------
+    start, stop : tuple of ints
+        indices in energies to get nearest to wv1, wv2
     """
     start = np.argmin(np.abs(energies - wv1))
     stop = np.argmin(np.abs(energies - wv2))
@@ -46,21 +53,51 @@ def custom_function(
     diffpeaks: bool = False,
 ):
     """ "
-    Function to build custom lmfit model for an arbitrary spectra given spectra data, number of peaks to fit,
-    function to fit background, function to fit peaks (currently all peaks fit with same function)
-
-    Args:
-        x: xdata / energies
-        y: ydata / spectra to fit
-        npeaks (int): Number of peaks to fit
-        peakfunction (lmfit.models): Model to fit all peaks, typically LorenzianModel, GaussianModel, VoigtModel
-        backgroundfuction (lmfit.models): Model to fit background, typically ExponentialModel, LinearModel
-        centers (list): Initial guess for peak centers
-        peaktol (int or float): Min/max range for peak center in fitting
-        diffpeaks (bool): If you want to fit each peak to specific lorentzian/gaussian/voigt model
-
-    Returns:
-        out (lmfit.model.ModelResult): Model result
+        Build a custom lmfit model for fitting arbitrary spectra data.
+    This function constructs a composite lmfit model to fit spectra data,
+    including background and peaks. All peaks are fit using the same
+    specified peak function unless `diffpeaks` is set to True.
+    Parameters
+    ----------
+    x : np.ndarray
+        The x-axis data (e.g., energies).
+    y : np.ndarray
+        The y-axis data (e.g., spectra to fit).
+    npeaks : int
+        Number of peaks to fit.
+    peakfunction : lmfit.models.Model
+        Model to fit all peaks (e.g., LorentzianModel, GaussianModel, VoigtModel).
+    backgroundfunction : lmfit.models.Model
+        Model to fit the background (e.g., ExponentialModel, LinearModel).
+    centers : list or np.ndarray
+        Initial guesses for the peak centers.
+    peaktol : float or int, optional, default=100
+        Minimum and maximum range for peak center fitting.
+    diffpeaks : bool, optional, default=False
+        If True, fits each peak to a specific model instead of using the same
+        model for all peaks.
+    Returns
+    -------
+    lmfit.model.ModelResult
+        The result of the model fitting.
+    Notes
+    -----
+    - The `backgroundfunction` is used to model the background of the spectra.
+    - If `diffpeaks` is False, all peaks are fit using the same `peakfunction`.
+    - Constraints are applied to the peak parameters, such as amplitude and
+      center bounds, based on the provided `centers` and `peaktol`.
+    Examples
+    --------
+    >>> from lmfit.models import GaussianModel, LinearModel
+    >>> import numpy as np
+    >>> x = np.linspace(0, 10, 100)
+    >>> y = np.sin(x) + np.random.normal(0, 0.1, size=x.size)
+    >>> centers = [2, 5, 8]
+    >>> result = custom_function(
+    ...     x, y, npeaks=3, peakfunction=GaussianModel,
+    ...     backgroundfunction=LinearModel, centers=centers
+    ... )
+    >>> print(result.fit_report())
     """
 
     bg_pre_dir = {ExponentialModel: "bgexp_", LinearModel: "bglin_"}
@@ -105,19 +142,47 @@ def fit_E_A(
     Fit on a custom range
     The out result from custom func actually contains all the fitting parameters, here just looking at the peak locations, but may be worthwile to store
     all the pars if needed later, since they are available?
-hyper
-    Args:
-        wv1: starting cm-1 of fit
-        wv2: ending cm-1 of fit
-        peakfunction (lmfit.models): Model to fit all peaks, typically LorenzianModel, GaussianModel, VoigtModel
-        backgroundfuction (lmfit.models): Model to fit background, typically ExponentialModel, LinearModel
-    Returns:
-        peak1: E' fitted peak location
-        peak1_err: Error on E'
-        peak2: A' fitted peak location
-        peak2_err: Error on A'
-        x, y : selected raw data x and y to fit
-        out.best_fit: best fit
+    Parameters
+    ----------
+    wl : array like
+        1D array of energies
+    spec : array like
+        1D array of spectra to fit
+    wv1 : float or int, optional, default=350
+        starting cm-1 of fit
+    wv2 : float or int, optional, default=450
+        ending cm-1 of fit
+    peakfunction : lmfit.models.Model, optional, default=GaussianModel
+        Model to fit all peaks, typically LorenzianModel, GaussianModel, VoigtModel
+    backgroundfunction : lmfit.models.Model, optional, default=ExponentialModel
+        Model to fit background, typically ExponentialModel, LinearModel
+    return_height : bool, optional, default=False
+        If True, also return the height of the fitted peaks.
+
+    Returns
+    --------
+    peak1 : float
+        E' fitted peak location
+    peak1_err : float
+        Error on E'
+    peak2 : float
+        A' fitted peak location
+    peak2_err : float
+        Error on A'
+    x : array like
+        selected raw data x to fit
+    y : array like
+        selected raw data y to fit
+    out.best_fit : array like
+        best fit of the data
+    peak1height : float, optional
+        height of the first peak if return_height is True
+    peak1height_err : float, optional
+        error on the height of the first peak if return_height is True
+    peak2height : float, optional
+        height of the second peak if return_height is True
+    peak2height_err : float, optional
+        error on the height of the second peak if return_height is True
 
     """
     start, stop = wv_range(wl, wv1, wv2)
@@ -165,6 +230,158 @@ hyper
             out.best_fit,
         )
     return peak1, peak1_err, peak2, peak2_err, x, y, out.best_fit
+
+
+def get_spot_size(
+    image, name, initial_guess=[100, 100, 100, 10, 10, 1, 0.0], scale=px_to_um_100
+):
+    """
+    Fits a 2D Gaussian to the laser spot image to determine the spot size.
+
+    Parameters
+    ----------
+    image : array like
+        2D array of the laser spot picture (image)
+    name : str
+        name of the sample
+    initial_guess : list, optional
+        initial guess for the Gaussian fit parameters, default is [100, 100, 100, 10, 10, 1, 0.0]
+    scale : float, optional
+        scaling factor for the spot size, default is px_to_um_100
+
+    Returns
+    -------
+    sigx : float
+        x standard deviation of the Gaussian fit (scaled from pixels to real units)
+    sigy : float
+        y standard deviation of the Gaussian fit (scaled from pixels to real units)
+    sigx_err : float
+        error on x standard deviation (scaled from pixels to real units)
+    sigy_err : float
+        error on y standard deviation (scaled from pixels to real units)
+
+    """
+    f, (ax1, ax2) = plt.subplots(1, 2)
+    cropped_img = crop_spot_pic(rgb2gray(image), padding=100)
+    xx, yy = np.indices(cropped_img.shape)
+    ax1.pcolormesh(xx, yy, cropped_img)
+    zz = cropped_img.ravel()
+    popt, pcov = curve_fit(Gaussian_2D_curvefit, (xx, yy), zz, p0=initial_guess)
+    fit = Gaussian_2D_curvefit((xx, yy), *popt).reshape(xx.shape)
+    sigx = popt[3]
+    sigy = popt[4]
+    sigx /= scale
+    sigy /= scale
+    perr = np.sqrt(np.diag(pcov))
+    sigx_err = perr[3]
+    sigy_err = perr[4]
+    sigx_err /= scale
+    sigy_err /= scale
+    ax2.pcolormesh(xx, yy, fit)
+    ax2.set_title(
+        rf"$\sigma_x=${round(sigx*2,2)} $\mu$m, $\sigma_y=${round(sigy*2,2)} $\mu$m"
+    )
+    f.suptitle(name)
+    return sigx * 2, sigy * 2, sigx_err * 2, sigy_err * 2
+
+
+def fit_dwdp(pwr, peak, peak_err=None, fit_err=False):
+    """
+    Fit series of peaks to a linear function to get the slope and intercept.
+
+    Parameters
+    ----------
+    pwr : array like
+        1D array of power values
+    peak : array like
+        1D array of peak values
+    peak_err : array like, optional
+        1D array of peak errors, default is None
+    fit_err : bool, optional
+        If True, fit with the errors, default is False
+
+    Returns
+    -------
+    popt : array like
+        Fitted parameters (slope and intercept)
+    perr : array like
+        Errors on the fitted parameters (slope and intercept)
+    """
+    if fit_err:
+        popt, pcov = curve_fit(linear, pwr, peak, sigma=peak_err, absolute_sigma=True)
+    else:
+        popt, pcov = curve_fit(linear, pwr, peak)
+    perr = np.sqrt(np.diag(pcov))
+    return popt, perr
+
+
+####
+### Fitting functions for Raja Lab / Molecular Foundry Data
+
+
+def fit_sweepmap(fn, xaxis, do_dwdp=True, remove_spikes=False, wls=None, plot=True):
+    file = h5.File(fn)
+    name = dict(file["/app/settings"].attrs.items())["sample"]
+    named_position = dict(file["/hardware/nd_wheel/settings"].attrs.items())[
+        "named_position"
+    ]
+    angle = dict(file["/hardware/power_wheel/settings"].attrs.items())["position"]
+    if wls is None:
+        wls = np.array(file["/measurement/hyperspec_picam_mcl_sweep/raman_shifts"])
+
+    spec_map = np.array(file["/measurement/hyperspec_picam_mcl_sweep/spec_map"])
+    scan_index_array = np.array(
+        file["/measurement/hyperspec_picam_mcl_sweep/scan_index_array"]
+    )
+    exposure = dict(file["/hardware/picam/settings"].attrs.items())["ExposureTime"]
+    dz, dx, dy, dp, dwv = spec_map.shape
+    if plot:
+        f, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    Aarr = []
+    Aarr_err = []
+    skip_index = []
+    for i in range(dp):
+        temp = spec_map[0][:, :, i].reshape(dy * dx, dwv)
+        av = temp.mean(axis=0)
+        _, _, A, Aerr, x, y, out = fit_E_A(wls, av, peakfunction=GaussianModel)
+        if plot:
+            ax1.plot(x, y + i * 5)
+            ax1.plot(x, out + i * 5)
+            ax1.set_xlim(350, 450)
+            # ax1.set_ylim(1300,np.max(out)+100)
+            ax2.errorbar(xaxis[i], A, Aerr, marker="o", color="blue")
+        if Aerr == 0 or Aerr == np.NaN:
+            if do_dwdp:
+                skip_index.append(i)
+            else:
+                skip_index.append(i)
+                Aarr.append(A)
+                Aarr_err.append(Aerr)
+
+        else:
+            Aarr.append(A)
+            Aarr_err.append(Aerr)
+
+    Aarr = np.array(Aarr)
+    Aarr_err = np.array(Aarr_err)
+    xindex = np.arange(dp)
+    xindex = list(set(xindex).difference(set(skip_index)))
+
+    xaxis = [xaxis[i] for i in xindex]
+    xaxis = np.array(xaxis)
+    if do_dwdp:
+        popt, perr = fit_dwdp(xaxis, Aarr, Aarr_err)
+        if plot:
+            ax2.plot(
+                xaxis,
+                linear(xaxis, *popt),
+                label=str(round(popt[0], 4)) + "+/-" + str(round(perr[0], 4)),
+            )
+            ax2.legend()
+            f.suptitle(name + "|" + str(exposure / 1e3) + "s")
+        return Aarr, Aarr_err, popt, perr, xaxis
+    else:
+        return Aarr, Aarr_err, xaxis
 
 
 def fit_multiple(
@@ -256,104 +473,3 @@ def fit_multiple_sweepmap(
         ax2.plot(xaxis[mask], linear(xaxis[mask], *popt), label=round(popt[0], 4))
         ax2.legend()
     return A_arr, Aerr_arr
-
-
-def get_spot_size(
-    image, name, initial_guess=[100, 100, 100, 10, 10, 1, 0.0], scale=px_to_um_100
-):
-    f, (ax1, ax2) = plt.subplots(1, 2)
-    cropped_img = crop_spot_pic(rgb2gray(image), padding=100)
-    xx, yy = np.indices(cropped_img.shape)
-    ax1.pcolormesh(xx, yy, cropped_img)
-    zz = cropped_img.ravel()
-    popt, pcov = curve_fit(Gaussian_2D_curvefit, (xx, yy), zz, p0=initial_guess)
-    fit = Gaussian_2D_curvefit((xx, yy), *popt).reshape(xx.shape)
-    sigx = popt[3]
-    sigy = popt[4]
-    sigx /= scale
-    sigy /= scale
-    perr = np.sqrt(np.diag(pcov))
-    sigx_err = perr[3]
-    sigy_err = perr[4]
-    sigx_err /= scale
-    sigy_err /= scale
-    ax2.pcolormesh(xx, yy, fit)
-    ax2.set_title(
-        rf"$\sigma_x=${round(sigx*2,2)} $\mu$m, $\sigma_y=${round(sigy*2,2)} $\mu$m"
-    )
-    f.suptitle(name)
-    return sigx*2, sigy*2, sigx_err*2, sigy_err*2
-
-
-def fit_dwdp(pwr, peak, peak_err=None, fit_err=False):
-    if fit_err:
-        popt, pcov = curve_fit(linear, pwr, peak, sigma=peak_err, absolute_sigma=True)
-    else:
-        popt, pcov = curve_fit(linear, pwr, peak)
-    perr = np.sqrt(np.diag(pcov))
-    return popt, perr
-
-
-def fit_sweepmap(fn, xaxis, do_dwdp=True, remove_spikes=False, wls=None, plot=True):
-    file = h5.File(fn)
-    name = dict(file["/app/settings"].attrs.items())["sample"]
-    named_position = dict(file["/hardware/nd_wheel/settings"].attrs.items())[
-        "named_position"
-    ]
-    angle = dict(file["/hardware/power_wheel/settings"].attrs.items())["position"]
-    if wls is None:
-        wls = np.array(file["/measurement/hyperspec_picam_mcl_sweep/raman_shifts"])
-
-    spec_map = np.array(file["/measurement/hyperspec_picam_mcl_sweep/spec_map"])
-    scan_index_array = np.array(
-        file["/measurement/hyperspec_picam_mcl_sweep/scan_index_array"]
-    )
-    exposure = dict(file["/hardware/picam/settings"].attrs.items())["ExposureTime"]
-    dz, dx, dy, dp, dwv = spec_map.shape
-    if plot:
-        f, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-    Aarr = []
-    Aarr_err = []
-    skip_index = []
-    for i in range(dp):
-        temp = spec_map[0][:, :, i].reshape(dy * dx, dwv)
-        av = temp.mean(axis=0)
-        _, _, A, Aerr, x, y, out = fit_E_A(wls, av, peakfunction=GaussianModel)
-        if plot:
-            ax1.plot(x, y + i * 5)
-            ax1.plot(x, out + i * 5)
-            ax1.set_xlim(350, 450)
-            # ax1.set_ylim(1300,np.max(out)+100)
-            ax2.errorbar(xaxis[i], A, Aerr, marker="o", color="blue")
-        if Aerr == 0 or Aerr == np.NaN:
-            if do_dwdp:
-                skip_index.append(i)
-            else:
-                skip_index.append(i)
-                Aarr.append(A)
-                Aarr_err.append(Aerr)
-
-        else:
-            Aarr.append(A)
-            Aarr_err.append(Aerr)
-
-    Aarr = np.array(Aarr)
-    Aarr_err = np.array(Aarr_err)
-    xindex = np.arange(dp)
-    xindex = list(set(xindex).difference(set(skip_index)))
-
-    xaxis = [xaxis[i] for i in xindex]
-    xaxis = np.array(xaxis)
-    if do_dwdp:
-        popt, perr = fit_dwdp(xaxis, Aarr, Aarr_err)
-        if plot:
-            ax2.plot(
-                xaxis,
-                linear(xaxis, *popt),
-                label=str(round(popt[0], 4)) + "+/-" + str(round(perr[0], 4)),
-            )
-            ax2.legend()
-            f.suptitle(name + "|" + str(exposure / 1e3) + "s")
-        return Aarr, Aarr_err, popt, perr, xaxis
-    else:
-        return Aarr, Aarr_err, xaxis
--- src/OTRaPy/DataAnalysis/ProcessedData.py
+++ src/OTRaPy/DataAnalysis/ProcessedData.py
@@ -5,7 +5,28 @@ import datetime
 import json
 
 
+## IN PROCESS ##
 class ProcessedData:
+    """
+    Class to store processed data for Raman Solver.
+
+    Attributes
+    ----------
+    database : dict
+        Dictionary to store processed data.
+    date : str
+        Date of the data collection.
+
+    Methods
+    ---------
+    add_raman_row : None
+        Adds a row to the database for Raman data.
+    add_tdtr_row : None
+        Adds a row to the database for TDTR data.
+    save : None
+        Saves the database to a json file.
+    """
+
     def __init__(self) -> None:
         self.database = {}
         t = datetime.datetime.today()
@@ -22,20 +43,23 @@ class ProcessedData:
         pwr,
         dwdp,
         dwdp_err,
-        dwdT, 
+        dwdT,
         dwdT_err,
-        dTdQ, 
-        dTdQ_err, 
+        dTdQ,
+        dTdQ_err,
         w0=0,
         w0_err=0,
         l0=0,
         l0_err=0,
-        h=3E-9,
+        h=3e-9,
         alpha=0.05,
         aniso=True,
         parallel_to_wrinkles=True,
         notes="",
     ):
+        """
+        Adds a row to the database for Raman data.
+        """
         self.date = date
         this_row = {
             "sample": sample,
@@ -48,15 +72,17 @@ class ProcessedData:
             "pwr": pwr,
             "dwdp": dwdp,
             "dwdp_err": dwdp_err,
-            "dwdT":dwdT, 
-            "dwdT_err":dwdT_err,
-            "dTdQ":dTdQ, 
-            "dTdQ_err":dTdQ_err, 
+            "dwdT": dwdT,
+            "dwdT_err": dwdT_err,
+            "dTdQ": dTdQ,
+            "dTdQ_err": dTdQ_err,
             "w0": w0,
             "w0_err": w0_err,
             "l0": l0,
             "l0_err": l0_err,
             "aniso": aniso,
+            "h": h,
+            "alpha": alpha,
             "paralell_to_wrinkles": parallel_to_wrinkles,
             "notes": notes,
         }
@@ -76,10 +102,12 @@ class ProcessedData:
         w0_err=0,
         w1=0,
         w1_err=0,
-        h=3E-9,
-
+        h=3e-9,
         notes="",
     ):
+        """
+        Adds a row to the database for TDTR data.
+        """
         self.date = date
         this_row = {
             "sample": sample,
@@ -93,15 +121,18 @@ class ProcessedData:
             "w0": w0,
             "w0_err": w0_err,
             "w1": w1,
-            "w1_err": l0_err,
-            "h" : h, 
-            "notes" : notes
+            "w1_err": w1_err,
+            "h": h,
+            "notes": notes,
         }
         self.database[sample + "_" + date] = this_row
 
     def save(self, save_path=None):
+        """
+        Saves the database to a json file.
+        """
         if save_path is None:
-            save_path = f"G:/My Drive/Raman_Data/Nanobubbles/{self.date}/"
+            save_path = f"/{self.date}/"
 
         with open(save_path + f"processed_data.json", "w") as f:
             json.dump(self.database, f)
